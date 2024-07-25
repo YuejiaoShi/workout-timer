97c8367dd11ae105fc5355f899f15bf42929dd24
YuejiaoShi <shiyuejiaosyj@gmail.com>
2024-07-26 00:06:45 +0200

    instead of make playSound useCallback, make it into a effect and accept [duration, allowSound] as dependency

--- src/Calculator.js
+++ src/Calculator.js
@@ -9,35 +9,44 @@ function Calculator({ workouts, allowSound }) {
 
   const [duration, setDuration] = useState(0);
 
-  // const duration = (number * sets * speed) / 60 + (sets - 1) * durationBreak;
-  const mins = Math.floor(duration);
-  const seconds = (duration - mins) * 60;
-
-  const playSound = useCallback(
-    () =>
-      function () {
-        if (!allowSound) return;
-        const sound = new Audio(clickSound);
-        sound.play();
-      },
-    [allowSound]
+  useEffect(
+    function () {
+      setDuration((number * sets * speed) / 60 + (sets - 1) * durationBreak);
+    },
+    [number, sets, speed, durationBreak]
   );
 
   useEffect(
     function () {
-      setDuration((number * sets * speed) / 60 + (sets - 1) * durationBreak);
+      const playSound = function () {
+        if (!allowSound) return;
+        const sound = new Audio(clickSound);
+        sound.play();
+      };
       playSound();
     },
-    [number, sets, speed, durationBreak, playSound]
+    [duration, allowSound]
   );
+
+  // const duration = (number * sets * speed) / 60 + (sets - 1) * durationBreak;
+  const mins = Math.floor(duration);
+  const seconds = (duration - mins) * 60;
+
+  // const playSound = useCallback(
+  //   () =>
+  //     function () {
+  //       if (!allowSound) return;
+  //       const sound = new Audio(clickSound);
+  //       sound.play();
+  //     },
+  //   [allowSound]
+  // );
 
   function handleInc() {
     setDuration((duration) => Math.floor(duration + 1));
-    playSound();
   }
   function handleDec() {
     setDuration((duration) => (duration > 1 ? Math.ceil(duration - 1) : 0));
-    playSound();
   }
 
   return (
